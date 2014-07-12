69e256ba0ce060cf3305f3de04316148fd778dbc
sugyan <sugi1982@gmail.com>
2014-07-12 22:29:48 +0900

    fix test

--- zenrizer_test.go
+++ zenrizer_test.go
@@ -5,22 +5,34 @@ import (
 )
 
 func TestZenrize(t *testing.T) {
-	var result string
+	var (
+		result string
+		err    error
+	)
 
 	// 城の崎にて
-	result = Zenrize("山の手線に跳ね飛ばされて怪我をした、その後養生に、一人で但馬の城崎温泉へ出掛けた。")
+	result, err = Zenrize("山の手線に跳ね飛ばされて怪我をした、その後養生に、一人で但馬の城崎温泉へ出掛けた。")
+	if err != nil {
+		t.Error(err)
+	}
 	if result != "山の手線に全裸で跳ね飛ばされて怪我を全裸でした、その後養生に、一人で但馬の城崎温泉へ全裸で出掛けた。" {
 		t.Errorf("城の崎にて: %s", result)
 	}
 
 	// ジョジョ
-	result = Zenrize("お前は今まで食ったパンの枚数を覚えているのか？")
+	result, err = Zenrize("お前は今まで食ったパンの枚数を覚えているのか？")
+	if err != nil {
+		t.Error(err)
+	}
 	if result != "お前は今まで全裸で食ったパンの枚数を全裸で覚えているのか？" {
 		t.Errorf("ジョジョ: %s", result)
 	}
 
 	// スラムダンク
-	result = Zenrize("あきらめたらそこで試合終了だよ")
+	result, err = Zenrize("あきらめたらそこで試合終了だよ")
+	if err != nil {
+		t.Error(err)
+	}
 	if result != "全裸であきらめたらそこで試合終了だよ" {
 		t.Errorf("スラムダンク: %s", result)
 	}
