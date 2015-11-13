922e677326b44f47cf9f0178848ca0a53f6f1d02
sugyan <sugi1982@gmail.com>
2015-11-14 00:16:32 +0900

    update logic

--- zenrizer.go
+++ zenrizer.go
@@ -33,13 +33,13 @@ func (z *Zenrizer) Zenrize(input string) (result string) {
 		// フラグが立っていれば「全裸で」を挿入
 		if verb {
 			insert := true
-			// 名詞／副詞／動詞のときはまだ挿入しない
-			// また、連用形の動詞→助(動)詞の場合も挿入しない
-			if features[0] == "名詞" || features[0] == "副詞" || features[0] == "動詞" {
+			// 名詞／副詞／動詞／助動詞 のときはまだ挿入しない
+			// また、連用形の動詞→助詞の場合も挿入しない
+			if features[0] == "名詞" || features[0] == "副詞" || features[0] == "動詞" || features[0] == "助動詞" {
 				insert = false
-			} else if (features[0] == "助詞" || features[0] == "助動詞") && i < len(tokens)-1 {
+			} else if features[0] == "助詞" && i < len(tokens)-1 {
 				prev := tokens[len(tokens)-i-2].Features()
-				if prev[5] == "連用形" {
+				if prev[5] == "連用形" || prev[5] == "連用タ接続" {
 					insert = false
 				}
 			}
